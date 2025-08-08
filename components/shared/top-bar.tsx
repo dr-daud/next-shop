27a575707da5cadaa4788c57b4472dd90b57e573
Daud <daud_edilbiev@mail.ru>
2025-08-08 17:42:40 +0300

    add sort & top-bar components

---
+++ components/shared/top-bar.tsx
@@ -0,0 +1,22 @@
+import { cn } from "@/lib/utils";
+import { Container, Categories, SortPopup } from "./index";
+
+interface Props {
+  className?: string;
+}
+
+export const TopBar: React.FC<Props> = ({ className }) => {
+  return (
+    <div
+      className={cn(
+        "sticky top-0 bg-white py-5 shadow-lg shadow-black/5 z-10",
+        className
+      )}
+    >
+      <Container>
+        <Categories />
+        <SortPopup />
+      </Container>
+    </div>
+  );
+};
