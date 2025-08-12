d0119bb27d8efee576a231562c84f78a0176861c
Daud <daud_edilbiev@mail.ru>
2025-08-12 17:57:22 +0300

    add zustand, create ProductGroupList

--- app/page.tsx
+++ app/page.tsx
@@ -1,4 +1,5 @@
 import { Container, Filters, Title, TopBar } from "@/components/shared";
+import { ProductsGroupList } from "@/components/shared/products-group-list";
 
 export default function Home() {
   return (
@@ -8,13 +9,28 @@ export default function Home() {
       </Container>
       <TopBar />
       <Container className="mt-10 pb-14">
-        <div className="flex-gap-[60px]">
+        <div className="flex-gap-[80px]">
           <div className="w-[250px]">
             <Filters />
           </div>
 
           <div className="flex-1">
-            <div className="flex flex-col gap-16">Список товаров</div>
+            <div className="flex flex-col gap-16">
+              <ProductsGroupList
+                title={"Пиццы"}
+                items={[
+                  {
+                    id: 1,
+                    name: "pal",
+                    imageUrl:
+                      "https://media.dodostatic.net/image/r:584x584/0197d0f934c372c3b17f812f340dc1ca.avif",
+                    price: 600,
+                    items: [{ price: 550 }],
+                  },
+                ]}
+                categoryId={1}
+              />
+            </div>
           </div>
         </div>
       </Container>
