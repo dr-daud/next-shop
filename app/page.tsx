04812aa3eef08041ffd7c0d92386c28d867622c1
Daud <daud_edilbiev@mail.ru>
2025-09-20 17:59:43 +0300

    fetch data from db

--- app/page.tsx
+++ app/page.tsx
@@ -1,13 +1,29 @@
 import { Container, Filters, Title, TopBar } from "@/components/shared";
 import { ProductsGroupList } from "@/components/shared/products-group-list";
+import { prisma } from "@/prisma/prisma-client";
 
-export default function Home() {
+export default async function Home() {
+  const categories = await prisma.category.findMany({
+    include: {
+      products: {
+        include: {
+          ingredients: true,
+          items: true,
+        },
+      },
+    },
+  });
+
   return (
     <>
       <Container className="mt-10">
         <Title text="Все пиццы" size="lg" className="font-extrabold" />
       </Container>
-      <TopBar />
+      <TopBar
+        categories={categories.filter(
+          (category) => category.products.length > 0
+        )}
+      />
       <Container className="mt-10 pb-14">
         <div className="flex-gap-[80px]">
           <div className="w-[250px]">
@@ -16,20 +32,17 @@ export default function Home() {
 
           <div className="flex-1">
             <div className="flex flex-col gap-16">
-              <ProductsGroupList
-                title={"Пиццы"}
-                items={[
-                  {
-                    id: 1,
-                    name: "pal",
-                    imageUrl:
-                      "https://media.dodostatic.net/image/r:584x584/0197d0f934c372c3b17f812f340dc1ca.avif",
-                    price: 600,
-                    items: [{ price: 550 }],
-                  },
-                ]}
-                categoryId={1}
-              />
+              {categories.map(
+                (category) =>
+                  category.products.length > 0 && (
+                    <ProductsGroupList
+                      key={category.id}
+                      title={category.name}
+                      categoryId={category.id}
+                      items={category.products}
+                    />
+                  )
+              )}
             </div>
           </div>
         </div>
--- components/shared/top-bar.tsx
+++ components/shared/top-bar.tsx
@@ -1,20 +1,21 @@
 import { cn } from "@/lib/utils";
 import { Container, Categories, SortPopup } from "./index";
+import { Category } from "@prisma/client";
 
 interface Props {
+  categories: Category[];
   className?: string;
 }
 
-export const TopBar: React.FC<Props> = ({ className }) => {
+export const TopBar: React.FC<Props> = ({ categories, className }) => {
   return (
     <div
       className={cn(
         "sticky top-0 bg-white py-5 shadow-lg shadow-black/5 z-10",
         className
-      )}
-    >
+      )}>
       <Container>
-        <Categories />
+        <Categories items={categories} />
         <SortPopup />
       </Container>
     </div>
